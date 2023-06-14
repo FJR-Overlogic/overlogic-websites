cc6ee10cee043f20a88bdbe63eeead1c432e1f3e
MFL4 <mhdfarhanl6267@gmail.com>
2023-06-14 12:07:47 +0700

    Create a Navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,5 +1,38 @@
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <h1>Overlogic</h1>
+      </div>
+      <ul>
+        <li>
+          {" "}
+          <a href="#" target="_blank">
+            Home
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="#" target="_blank">
+            About
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="#" target="_blank">
+            Contact
+          </a>
+        </li>
+        <li id="github hre">
+          <a href="https://github.com/FJR-Overlogic/overlogic-websites" target="_blank">
+            Github
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
